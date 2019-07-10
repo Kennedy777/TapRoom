66c80a266a5a730012b70fe884c57e88e37c6906
Student <example@example.com>
2019-07-10 09:26:54 -0700

    revise rendering

--- src/components/AddKeg/NewKegForm.jsx
+++ src/components/AddKeg/NewKegForm.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import NewKegForm from './NewKegForm';
+import { v4 } from 'uuid'; 
 
-function NewKegControl(props){
-  return (
-    <div>
-      <NewKegForm onNewKegCreation={props.onNewKegCreation}/>
-    </div>
-  );
-}
+import Paper from '@material-ui/core/Paper';
+
+function NewKegForm(props){
+ let _name = null;
+ let _brand = null;
+ let _price = null; 
+ let _aBV = null;
+ let _img = null;
+
+ function handleNewKegFormSubmission(event) {
+   event.preventDefault(); 
+   props.onNewKegCreation({name:_name.value, brand: _brand.vale, price: _price.value, aBV: _aBV.value; img = _image.value, id: v4()});
+   _name.value = '';
+   _brand.value = '';
+   _price.value = '';
+   _aBV.value = '';
+   _img.value = ''; 
+ }
 
 NewKegControl.propTypes = {
   onNewKegCreation: PropTypes.func
